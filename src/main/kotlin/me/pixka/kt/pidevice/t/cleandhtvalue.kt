55707247268091780cd476a66d321cf89f544d97
Kritchai Phromros <ky@pixka.me>
2017-11-05 12:05:39 +0700

    สำหรับคุมโรงเสร็จแล้ว

--- src/main/kotlin/me/pixka/kt/pidevice/t/cleandhtvalue.kt
+++ src/main/kotlin/me/pixka/kt/pidevice/t/cleandhtvalue.kt
@@ -1,6 +1,7 @@
 package me.pixka.kt.pidevice.t
 
 import me.pixka.pibase.s.DhtvalueService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -11,5 +12,10 @@ class CleanDhtvalue(val dhts:DhtvalueService) {
     fun clean()
     {
         dhts.cleanToserver()
+        logger.info("Clean DHT value")
+
+    }
+    companion object {
+        internal var logger = LoggerFactory.getLogger(CleanDhtvalue::class.java)
     }
 }
--- src/main/kotlin/me/pixka/kt/pidevice/t/cleandsvalue.kt
+++ src/main/kotlin/me/pixka/kt/pidevice/t/cleandsvalue.kt
@@ -2,6 +2,7 @@ package me.pixka.kt.pidevice.t
 
 import me.pixka.pibase.s.DhtvalueService
 import me.pixka.pibase.s.Ds18valueService
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -13,5 +14,10 @@ class CleanDsvalue(val ds:Ds18valueService) {
     fun clean()
     {
         ds.cleanToserver()
+        logger.debug("Clean DS value ")
+    }
+
+    companion object {
+        internal var logger = LoggerFactory.getLogger(CleanDsvalue::class.java)
     }
 }
