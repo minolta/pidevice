33556502b1f96c02263ade355e6f2ec9440c144b
Kritchai Phromros <ky@pixka.me>
2020-10-06 13:34:40 +0700

    Test checkin
    Former-commit-id: 9d2406a7373c98334fe7a6d7e2d2f837c7a305a7

--- src/test/kotlin/me/pixka/kt/pidevice/TestCheckin.kt
+++ src/test/kotlin/me/pixka/kt/pidevice/TestCheckin.kt
@@ -3,6 +3,7 @@ package me.pixka.kt.pidevice
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import me.pixka.kt.pibase.d.Devicecheckin
+import me.pixka.kt.pibase.s.HttpService
 import me.pixka.pibase.o.Infoobj
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
@@ -14,43 +15,16 @@ import java.util.*
 
 class TestCheckin {
     var om = ObjectMapper()
-    fun checkin(ip: String): Devicecheckin {
+    val httpService =HttpService()
+    fun checkin(url: String): Devicecheckin {
         try {
-            var url = URL(ip)
-
-            var encoding = Base64.getEncoder().encodeToString("USER_CLIENT_APP:password".toByteArray())
-            //.encodeToString(("USER_CLIENT_APP:password").getBytes("UTF-8"));
-            println(encoding)
-            var c = url.openConnection() as HttpURLConnection
-            c.requestMethod = "POST"
-            c.connectTimeout = 2000
-            c.setRequestProperty("Content-Type", "application/json; utf-8")
-            c.setRequestProperty("Accept", "application/json")
-            c.setRequestProperty("Authorization", "Basic " + encoding)
-            c.doOutput = true
 
             val i = Infoobj()
             i.ip = System.getProperty("fixip")
             i.mac = System.getProperty("mac")
             i.password = UUID.randomUUID().toString() // สร้าง ยฟหหไนพก
-//            i.uptime = 10000;
-            // สำหรับ ให้
-            var jsonInputString = om.writeValueAsString(i)
-            println(jsonInputString)
-            c.outputStream.write(jsonInputString.toByteArray())
-            c.outputStream.close()
-//            c.outputStream.use({ os ->
-//                val input: ByteArray = jsonInputString.toByteArray(Charset.forName("UTF-8"))
-//                os.write(input, 0, input.size)
-//            })
-            val buf = BufferedReader(
-                    InputStreamReader(
-                            c.inputStream))
-            val response = StringBuilder()
-            var inputLine: String? = ""
-            while (buf.readLine().also({ inputLine = it }) != null)
-                response.append(inputLine)
-            var re = response.toString()
+            var re = httpService.post(url,i,2000)
+            println(re)
             var t = om.readValue<Devicecheckin>(re)
             return t
         } catch (e: Exception) {
@@ -60,6 +34,6 @@ class TestCheckin {
 
     @Test
     fun testCheckin() {
-        checkin("http://192.168.88.21:2222/checkin")
+        checkin("http://localhost:8080/checkin")
     }
 }
