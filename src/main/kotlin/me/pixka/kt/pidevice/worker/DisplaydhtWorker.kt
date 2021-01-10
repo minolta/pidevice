7e8e1c7c318f34bea25981677703407372c4d9d3
Kritchai Phromros <ky@pixka.me>
2021-01-10 11:04:49 +0700

    Add fix display DHT
    Former-commit-id: 20a5efed06c9f11c2b6e3488acd4dd04e96a1f28

--- src/main/kotlin/me/pixka/kt/pidevice/worker/DisplaydhtWorker.kt
+++ src/main/kotlin/me/pixka/kt/pidevice/worker/DisplaydhtWorker.kt
@@ -21,7 +21,7 @@ class DisplaydhtWorker(pijob: Pijob, var mtp: MactoipService) : DWK(pijob), Runn
                     try {
                         var name = pijob.desdevice?.name
                         var ip = mtp.mactoip(it.device?.mac!!)
-                        var url = mtp.http.encode("ค่าความชืนของ ${name} = ${dht.h} อุณหภูมิ ${dht.t} ")
+                        var url = mtp.http.encode("ค่าความชืนของ ${name}:${dht.h} อุณหภูมิ:${dht.t} ")
                         var re = mtp.http.get("http://${ip}/settext?t=${url}&tn=2&l=4", 5000)
                         status = "http://${ip}/settext?t=${url}&tn=2&l=4"
                     } catch (e: Exception) {
