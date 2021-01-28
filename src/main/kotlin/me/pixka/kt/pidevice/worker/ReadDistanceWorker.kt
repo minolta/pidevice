a09a860ae11c3f109d516cc2c97590198d62235a
Kritchai Phromros <ky@pixka.me>
2021-01-29 06:08:47 +0700

    Change timeout
    Former-commit-id: 26fcea507bde05f0011fe85e3b8268893c7ea9a2

--- src/main/kotlin/me/pixka/kt/pidevice/worker/ReadDistanceWorker.kt
+++ src/main/kotlin/me/pixka/kt/pidevice/worker/ReadDistanceWorker.kt
@@ -11,8 +11,10 @@ import me.pixka.kt.run.DWK
 import org.slf4j.LoggerFactory
 import java.util.*
 
-class ReadDistanceWorker(job: Pijob, var mtp: MactoipService, var ds: DistanceService,
-                         val pideviceService: PideviceService) : DWK(job), Runnable {
+class ReadDistanceWorker(
+    job: Pijob, var mtp: MactoipService, var ds: DistanceService,
+    val pideviceService: PideviceService
+) : DWK(job), Runnable {
     override fun run() {
         isRun = true
         startRun = Date()
@@ -24,7 +26,7 @@ class ReadDistanceWorker(job: Pijob, var mtp: MactoipService, var ds: DistanceSe
                 var ip = mtp.mactoip(mac)
 
                 if (ip != null) {
-                    var re = mtp.http.get("http://${ip}", 5000)
+                    var re = mtp.http.get("http://${ip}", 60000)
                     status = "Read Distance from ${pijob.desdevice?.name}"
                     var d = mtp.om.readValue<DistanceObj>(re)
                     var distance = Distance()
@@ -37,29 +39,35 @@ class ReadDistanceWorker(job: Pijob, var mtp: MactoipService, var ds: DistanceSe
                     status = "End read ${d.distance}"
                     exitdate = findExitdate(pijob)
                 } else {
-                    mtp.lgs.createERROR("No ip ", Date(),
-                            "ReadDistanceWorker", "", "26", "run()", mac, pijob.refid, pijob.pidevice_id)
+                    logger.error("Error Read No ip ${pijob.name}")
+                    mtp.lgs.createERROR(
+                        "No ip ", Date(),
+                        "ReadDistanceWorker", "", "26", "run()", mac, pijob.refid, pijob.pidevice_id
+                    )
                     isRun = false
 
                 }
             } else {
-                mtp.lgs.createERROR("No mac", Date(),
-                        "ReadDistanceWorker", "", "23", "run()", mac, pijob.refid, pijob.pidevice_id)
+                logger.error("Error Read No Mac address ${pijob.name}")
+
+                mtp.lgs.createERROR(
+                    "No mac", Date(),
+                    "ReadDistanceWorker", "", "23", "run()", mac, pijob.refid, pijob.pidevice_id
+                )
                 isRun = false
 
             }
         } catch (e: Exception) {
+            logger.error(" Distance READ Time out ${pijob.name}  ERROR:${e.message}")
             mtp.lgs.createERROR(
-                    "${e.message}", Date(),
-                    "ReadDistanceWorker", "", "13", "run()", mac, pijob.refid
+                "${e.message}", Date(),
+                "ReadDistanceWorker", "", "13", "run()", mac, pijob.refid
             )
-            logger.error(e.message)
+
             isRun = false
         }
     }
 
-    companion object {
-        internal var logger = LoggerFactory.getLogger(ReadDistanceWorker::class.java)
-    }
+         var logger = LoggerFactory.getLogger(ReadDistanceWorker::class.java)
 }
 
