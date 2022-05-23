928321c4146612b869937b3eed6495a15b6e49f8
Kritchai Phromros <ky@pixka.me>
2022-05-23 22:14:27 +0700

    Fix respressure
    Former-commit-id: 01372a6fb46d6ebb2ec5512bef77c0e5512d933c

---
+++ src/test/kotlin/me/pixka/kt/pidevice/d/Testcheckalready.kt
@@ -0,0 +1,17 @@
+package me.pixka.kt.pidevice.d
+
+import me.pixka.kt.pibase.d.Pijob
+import org.junit.jupiter.api.Test
+
+class Testcheckalready {
+
+    @Test
+    fun testCheckfunction()
+    {
+        var data = ArrayList<Pijob>()
+
+//        var w1 =
+
+
+    }
+}
